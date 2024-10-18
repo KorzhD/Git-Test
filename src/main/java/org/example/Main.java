a183617fdaafd5f1cf6b6b72f2033597cd6fadac
KorzhD <korzhn78@gmail.com>
2024-10-18 16:06:22 +0300

    Update Main12345678

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        ArrayList<String> a = new ArrayList();
-        System.out.println("uwu");
-        System.out.println("UWUNYA");
-
-        System.out.println("uwu");
-    System.out.println("uwu123");
+        System.out.println("GITTTT");
     }
 }
