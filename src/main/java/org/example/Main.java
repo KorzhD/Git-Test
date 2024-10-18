00058faebba19e31d55d22132ef9fe97c523ce50
KorzhD <korzhn78@gmail.com>
2024-10-18 15:56:10 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         ArrayList<String> a = new ArrayList();
+        System.out.prtintln("uwu");
 
     }
 }
